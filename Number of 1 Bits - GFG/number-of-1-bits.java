284fea72146d4b66517e1b6e5d98d0c65849c806
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-04-02 14:09:42 +0530

    Added solution - LeetHub

--- Number of 1 Bits - GFG/number-of-1-bits.java
+++ Number of 1 Bits - GFG/number-of-1-bits.java
@@ -25,9 +25,8 @@ class Solution {
         // code here
         int count=0;
        while(N>0){
-           if((N&1)==1)
               count++;
-       N=N>>1;    
+              N = N&(N-1);
        }
        return count;
     }
