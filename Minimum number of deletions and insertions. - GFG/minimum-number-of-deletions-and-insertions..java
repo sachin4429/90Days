53dbc1726451863639b35a562feac6ae628da22b
sachin4429 <56739056+sachin4429@users.noreply.github.com>
2022-04-08 16:58:07 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..java
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..java
@@ -23,35 +23,22 @@ class GfG
 
 class Solution
 {
-	public int minOperations(String X, String Y) 
+	public int minOperations(String str1, String str2) 
 	{ 
-	   int m = X.length();
-	   int n = Y.length();
-	   int t[][] = new int[m+1][n+1];
-       for(int i = 1; i<m+1; i++)
-        {
-            for(int j = 1; j<n+1; j++)
-            {
-                if(X.charAt(i-1) == Y.charAt(j-1))
-                    t[i][j] = 1+t[i-1][j-1];
-                else
-                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
-            }
-        }
-        return n-t[m][n] + m-t[m][n];
-	   // int n = str1.length();
-	   // int m = str2.length();
-	   // int t[][] = new int[n+1][m+1];
-	   // for(int i = 1; i<n+1; i++)
-	   // {
-	   //     for(int j = 1; i<m+1; j++)
-	   //     {
-	   //         if(str1.charAt(i-1) == str2.charAt(j-1))
-	   //             t[i][j] = 1 + t[i-1][j-1];
-	   //        else
-	   //             t[i][j] = Math.max(t[i-1][j],t[i][j-1]);
-	   //     }
-	   // }
-	   // return n-t[n][m] + m-t[n][m];
+	    int m = str1.length();
+	    int n = str2.length();
+	    int t[][] = new int[m+1][n+1];
+	    for(int i = 1; i<m+1; i++)
+	    {
+	        for(int j = 1; j<n+1; j++)
+	        {
+	            if(str1.charAt(i-1) == str2.charAt(j-1))
+	                t[i][j] = 1+t[i-1][j-1];
+	           else
+	                t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+	        }
+	    }
+	    
+	    return m-t[m][n] + n-t[m][n];
 	} 
 }
